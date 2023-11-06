8e5d4e9bfb61c295c6d00458fd13f97caa045f87
Robson <robccorreia@gmail.com>
2023-11-06 18:10:25 -0300

    feat: add Routes + pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/theme/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Routes";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Olá</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/Home";
+import { CompleteOrderPage } from "./pages/CompleteOrder";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/completeOrder" element={<CompleteOrderPage />} />
+    </Routes>
+  );
+}
