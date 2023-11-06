15ed42015d555a88743167af432b28b944c099a2
Robson <robccorreia@gmail.com>
2023-11-06 18:05:41 -0300

    feat: add styles / theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/theme/global";
+
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Olá</h1>
+    </ThemeProvider>
+  );
 }
 
 export default App;
