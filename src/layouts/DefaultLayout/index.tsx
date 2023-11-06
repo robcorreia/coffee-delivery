4ecb144e94aeb049442d8cf0b24fa6edde84f7e9
Robson <robccorreia@gmail.com>
2023-11-06 19:20:51 -0300

    feat: add Header component

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,11 @@
+import { Header } from "../../components/Header";
 import { LayoutContainer } from "./styles";
 import { Outlet } from "react-router-dom";
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
-      <h2>header</h2>
+      <Header />
       <Outlet />
     </LayoutContainer>
   );
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
-export const LayoutContainer = styled.div``;
+export const LayoutContainer = styled.div`
+  .container {
+    width: 100%;
+    max-width: 78rem;
+    margin-right: auto;
+    margin-left: auto;
+  }
+`;
