d44ec7398d0c247c7eb9debf575013d0a680cdc2
Robson <robccorreia@gmail.com>
2023-11-13 20:23:44 -0300

    feat: add PaymentMethodInput component

---
+++ src/pages/CompleteOrder/components/PaymentMethodInput/index.tsx
@@ -0,0 +1,12 @@
+import { CreditCard } from "phosphor-react";
+import { PaymentMethodInputContainer } from "./styles";
+
+export function PaymentMethodInput() {
+  return (
+    <PaymentMethodInputContainer>
+      {" "}
+      <CreditCard size={16} />
+      Cartão de crédito
+    </PaymentMethodInputContainer>
+  );
+}
